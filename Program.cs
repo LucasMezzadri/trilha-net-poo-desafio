52288f037f53f133512b155ccd1611be212e36a3
Lucas Mezzadri <lucasmlopes1997@gmail.com>
2022-11-07 15:46:51 -0300

    Desafio Concluidogit add trilha-net-poo-desafiogit add trilha-net-poo-desafiogit add trilha-net-poo-desafio!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "123", modelo: "Tijolão", imei: "111", memoria: 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarApp("Snake Game");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone(numero: "789", modelo: "14 Pro Max", imei: "222", memoria: 4);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarApp("Instagram");
+
